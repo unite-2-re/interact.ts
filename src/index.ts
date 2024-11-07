54de8085fbc17c53e03c74028d0ea667e8e71d88
unit-404 <conway24b@gmail.com>
2024-11-07 15:57:11 +0700

    new sort of interaction

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import AxGesture from "./$core$/Gesture";
 
 //
+export * from "./$core$/Selection";
 export * from "./$core$/Zoom";
 export * from "./$core$/PointerAPI";
 export * from "./$core$/Gesture";
