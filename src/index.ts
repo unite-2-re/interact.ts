5cb82e846fd97c1a014ddc948f9ca880c6b422fd
u2re-dev <c24b@u2re.ru>
2025-04-22 18:43:13 +0700

    merge scroll

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ import AxGesture from "./$core$/Gesture";
 export * from "./$core$/Selection";
 export * from "./$core$/PointerAPI";
 export * from "./$core$/Gesture";
+export * from "./$core$/Scrollbar";
+
+//
 export default AxGesture;
